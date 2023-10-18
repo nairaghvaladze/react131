763421427533e7a0cfab169aabae8ba969f94800
nairaghvaladze <120264867+nairaghvaladze@users.noreply.github.com>
2023-10-18 21:41:31 +0400

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+function About()
+{
+    return(
+        <h1>ჩვენს შესახებ</h1>
+    )
+}
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,7 @@
+function Contact()
+{
+    return(
+        <h1>კონტაქტი</h1>
+    )
+}
+export default Contact;
