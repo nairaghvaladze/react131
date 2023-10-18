03ef95fe25519213ca757f603f582d6def879ec3
nairaghvaladze <120264867+nairaghvaladze@users.noreply.github.com>
2023-10-18 21:44:53 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component{
            <BrowserRouter>          
            <Routes>
            
-            <Route path="/" element={<Layout />} /> 
+            <Route index path="/" element={<Layout />} /> 
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
            </Routes>
