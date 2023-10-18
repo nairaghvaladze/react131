b981309e1a67d10e9279e45e2fe6d0bd93635208
nairaghvaladze <120264867+nairaghvaladze@users.noreply.github.com>
2023-10-18 21:07:53 +0400

    Add files via upload

---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import {Link, Outlet} from "react-router-dom";
+function Layout() {
+   <div>
+     <ul>
+    <li>
+        <Link to={"/"}> Home</Link>
+    </li>
+    <li>
+        <Link to={"/about"}>About</Link>
+    </li>
+    <li>
+        <Link to={"/contact"}>Contact</Link>
+    </li>
+   </ul>
+   <Outlet />
+   </div>
+}
+export default Layout;
